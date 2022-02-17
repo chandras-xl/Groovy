165f611b6b2715293e8009945b8f8013d3a6483d
Chandrashekhar S <chandrashekhar.s@agiliad.local>
2022-02-17 21:38:51 +0530

    initial commit

---
+++ Groovy-basic-programs/lengthofstring.groovy
@@ -0,0 +1,8 @@
+//Groovy - check the length of the string
+
+class StringLength{
+    static void main(String[] args){
+        String str = "Hello World!"
+        println("The string length is: "+str.length())
+    }
+}
